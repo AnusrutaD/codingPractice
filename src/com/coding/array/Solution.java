f57a6d244507e641b08f1465efea10ffba493db5
ANUSRUTA DUTTA <anusruta.dutta@gmail.com>
2022-08-07 17:34:06 +0530

    Update with dynamic array

--- src/com/coding/array/Solution.java
+++ src/com/coding/array/Solution.java
@@ -1,26 +1,31 @@
 package com.coding.array;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
 
 
     // Rain water trap problem
-    public int trap(int[] height) {
-        int size = height.length;
-        int[] leftMax = new int[size];
-        leftMax[0] = height[0];
+    public int trap(List<Integer> height) {
+        int size = height.size();
+        List<Integer> leftMax = new ArrayList<>();
+        leftMax.add(height.get(0));
         for(int i = 1; i < size; i++){
-            leftMax[i] = Math.max(leftMax[i -1], height[i]);
+            leftMax.add(Math.max(leftMax.get(i -1), height.get(i)));
         }
-        int[] rightMax = new int[size];
-        rightMax[size -1] = height[size - 1];
+        List<Integer> rightMax = new ArrayList<>();
+        rightMax.add(height.get(size - 1));
         for(int i = size - 2; i >= 0; i--){
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+            rightMax.get(Math.max(rightMax.get(i - 1), height.get(i)));
         }
+        Collections.reverse(rightMax);
         // System.out.println(Arrays.toString(leftIdx));
         // System.out.println(Arrays.toString(rightIdx));
         int ans = 0;
-        for(int i = 1; i < height.length - 1; i++){
-            ans += (Math.min(rightMax[i], leftMax[i]) - height[i]);
+        for(int i = 1; i < size - 1; i++){
+            ans += (Math.min(rightMax.get(i), leftMax.get(i)) - height.get(i));
         }
         return ans;
     }
